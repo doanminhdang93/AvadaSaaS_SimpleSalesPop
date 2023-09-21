2c2a2f523793a13a444d2f560dd4b64f74429028
Dmih_Dang9 <minhdang09032002@gmail.com>
2023-09-21 10:13:40 +0700

    Done

--- packages/functions/src/commands/testShopify.js
+++ packages/functions/src/commands/testShopify.js
@@ -1,29 +1,31 @@
 const Shopify = require('shopify-api-node');
-// const {getSetting} = require('../repositories/settingsRepository');
 
 (async () => {
   const shopify = new Shopify({
     shopName: 'avada-saas-training.myshopify.com',
-    accessToken: 'shpua_4e9b4b7b3a9d6520b2b724259c1f5100'
+    accessToken: 'shpua_36041d32f51b2e657b5221b834bb5cb3'
   });
-  const data = await shopify.theme.list();
-  // console.log(data);
+  // const data = await shopify.theme.list();
+  // // console.log(data);
 
-  const idTheme = data[0].id;
-  const templateJson = await shopify.asset.get(idTheme, {
-    asset: {key: 'config/settings_data.json'}
-  });
-  let templateValue = JSON.parse(templateJson.value);
-  let blocks = templateValue.current.blocks;
+  // const idTheme = data[0].id;
+  // const templateJson = await shopify.asset.get(idTheme, {
+  //   asset: {key: 'config/settings_data.json'}
+  // });
+  // let templateValue = JSON.parse(templateJson.value);
+  // let blocks = templateValue.current.blocks;
 
-  let updatedData = Object.fromEntries(
-    Object.entries(blocks).map(([key, value]) => [key, {...value, disabled: false}])
-  );
-  console.log(updatedData);
-  templateValue.current.blocks = updatedData;
+  // let updatedData = Object.fromEntries(
+  //   Object.entries(blocks).map(([key, value]) => [key, {...value, disabled: false}])
+  // );
+  // console.log(updatedData);
+  // templateValue.current.blocks = updatedData;
 
-  await shopify.asset.update(idTheme, {
-    key: 'config/settings_data.json',
-    value: JSON.stringify(templateValue)
-  });
+  // await shopify.asset.update(idTheme, {
+  //   key: 'config/settings_data.json',
+  //   value: JSON.stringify(templateValue)
+  // });
+
+  const webhooks = await shopify.webhook.list();
+  console.log(webhooks);
 })();
--- packages/scripttag/src/managers/ApiManager.js
+++ packages/scripttag/src/managers/ApiManager.js
@@ -3,13 +3,14 @@ import makeRequest from '../helpers/api/makeRequest';
 const BASE_URL = 'https://localhost:3000';
 export default class ApiManager {
   getNotifications = async () => {
-    return this.getApiData();
+    const {notifications, settings} = await this.getApiData();
+    return {notifications, settings};
   };
 
   getApiData = async () => {
     try {
       const response = await makeRequest(
-        `${BASE_URL}/clientApi/notifications?shopifyDomain=avada-saas-training.myshopify.com`
+        `${BASE_URL}/clientApi/notifications?shopifyDomain=${window.Shopify.shop}`
       );
       return response.data;
     } catch (error) {
